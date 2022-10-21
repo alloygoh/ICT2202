f64510e1458a65108c7e8d753e81f785f990f202
fwishyy <fwishytf2@gmail.com>
2022-10-21 10:46:59 +0800

    feat: add monitoring poc

--- ConsoleApplication1/ConsoleApplication1/keyboard.h
+++ ConsoleApplication1/ConsoleApplication1/keyboard.h
@@ -4,18 +4,28 @@
 #include <Windows.h>
 #include <psapi.h>
 #include <ctime>
+#include <map>
+#include <mutex>
+#include <string>
 #include <thread>
 #include <vector>
 
 LRESULT __stdcall hookCallback(int, WPARAM, LPARAM);
 bool releaseHook();
 bool setHook();
+int setModKeyState(int, int);
+char formatKey(char);
+
 void replayStoredKeystrokes();
 void keyboardHook();
 
 extern HHOOK ghHook;
 extern KBDLLHOOKSTRUCT kbdStruct;
+extern std::mutex keyboardHookMutex;
+extern std::string kbBuffer;
 extern std::vector<INPUT> vInputs;
+extern std::map<char, char> layeredKeys;
+extern std::map<int, std::string> mapSpecialKeys;
 extern bool ALLOW_INPUT;
 extern bool INPUT_BELOW_THRESHOLD;
 
